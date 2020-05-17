8fed4bfcaecbdece5190eca480877ad7209a3dcc
pbrimble <pbrimble@gmail.com>
2020-05-17 13:11:29 +0100

    update preamble

--- heterogeneous-treatment-effects/generic-machine-learning/gml-hte_an.R
+++ heterogeneous-treatment-effects/generic-machine-learning/gml-hte_an.R
@@ -1,29 +1,32 @@
-#--------------------------------------------------------------------------------------------------------------------
-# This program obtains empirical results for the paper "Generic Machine Learning Discovery
-# and Classification Analysis of Heterogenous Treatment Effects in Randomized Experiments"
-# by V. CHERNOZHUKOV, M. DEMIRER, E. DUFLO, I. FERNANDEZ-VAL
-#--------------------------------------------------------------------------------------------------------------------
-# Authors:  V. CHERNOZHUKOV, M. DEMIRER, E. DUFLO, I. FERNANDEZ-VAL
-#--------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------#
+# This programme applies the Generic Machine Learning for Heterogeneous Treatment Effects
+# methodology from Chornozhukhov et al. 2019. The code has been modified from previous code written by Demirer.
 # Several modifications have been made to make the code more flexible, including the introduction of
 # k-fold cross-validation and separating the machine learning code and analysis code into two files.
-#--------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------#
 # Authors:  Paul Brimble - Mind and Behaviour Research Group,
 #           Centre for the Study of African Economies (CSAE), University of Oxford
 # Email:    paul.brimble@bsg.ox.ac.uk
+#------------------------------------------------------------------------------#
+# Preamble for original code:
+# This program obtains empirical results for the paper "Generic Machine Learning Discovery
+# and Classification Analysis of Heterogenous Treatment Effects in Randomized Experiments"
+# by V. CHERNOZHUKOV, M. DEMIRER, E. DUFLO, I. FERNANDEZ-VAL
+# Authors:  V. CHERNOZHUKOV, M. DEMIRER, E. DUFLO, I. FERNANDEZ-VAL
+#------------------------------------------------------------------------------#
 
 # The code is split into two files:
-# 1) "ml" - machine learning estimation.
-# 2) "an" - analysis using inputs from ml.
+# 1) "gml-hte_ml" - machine learning estimation.
+# 2) "gml-hte_an" - analysis using inputs from "gml-hte_ml".
 
-# The "ml" code returns an RData file to be used in the "an" code
+# The "gml-hte_ml" code returns an RData file to be used in the "gml_hte_an" code
 
-# The "an" code returns five outputs
-# 1) Two plots for each outcome variable reporting ATE by quantile groups (first plot is top 2 ML methods, second plot is all ML methods)
-# 2) het_results: Latex table reporting ATE and heterogeneity loading coefficient
-# 3) test_results: A latex table reporting estimated ATE_group1 - ATE_group5 and its p-value
-# 4) index_ml_best: A latex table reporting index_ml_best ML methods
-# 5) RData file.
+# The "gml_hte_an" code returns five outputs:
+# 1) PLOTS: Two plots for each outcome variable reporting ATE and GATES (first plot is top 2 methods, second plot is all methods)
+# 2) BLP: Latex table reporting ATE and heterogeneity loading factor (HET).
+# 3) GATES: Latex table reporting the GATE for the most and least affected groups, and the difference.
+# 4) CLAN: Latex table reporting the most affected and least affected averages for the CLAN variables and the difference.
+# 5) BEST: Latex table reporting best machine learning methods.
 
 # ML_Functions.R code is also required.
 
--- heterogeneous-treatment-effects/generic-machine-learning/gml-hte_ml.R
+++ heterogeneous-treatment-effects/generic-machine-learning/gml-hte_ml.R
@@ -1,7 +1,6 @@
 #------------------------------------------------------------------------------#
 # This programme applies the Generic Machine Learning for Heterogeneous Treatment Effects
-# methodology from Chornozhukhov et al. 2019. The code has been modified from previous code
-# written by Demirer.
+# methodology from Chornozhukhov et al. 2019. The code has been modified from previous code written by Demirer.
 # Several modifications have been made to make the code more flexible, including the introduction of
 # k-fold cross-validation and separating the machine learning code and analysis code into two files.
 #------------------------------------------------------------------------------#
@@ -17,17 +16,17 @@
 #------------------------------------------------------------------------------#
 
 # The code is split into two files:
-# 1) "ml" - machine learning estimation.
-# 2) "an" - analysis using inputs from ml.
+# 1) "gml-hte_ml" - machine learning estimation.
+# 2) "gml-hte_an" - analysis using inputs from "gml-hte_ml".
 
 # The "gml-hte_ml" code returns an RData file to be used in the "gml_hte_an" code
 
-# The "gml_hte_an" code returns five outputs
-# 1) A plot for each outcome variable reporting ATE by quantile groups
-# 2) het_results: Latex table reporting ATE and heterogeneity loading coefficient
-# 3) test_results: A latex table reporting estimated ATE_group1 - ATE_group5 and its p-value
-# 4) best: A latex table reporting best ML methods
-# 5) MSE: A latex table of mean squared errors from tuning
+# The "gml_hte_an" code returns five outputs:
+# 1) PLOTS: Two plots for each outcome variable reporting ATE and GATES (first plot is top 2 methods, second plot is all methods)
+# 2) BLP: Latex table reporting ATE and heterogeneity loading factor (HET).
+# 3) GATES: Latex table reporting the GATE for the most and least affected groups, and the difference.
+# 4) CLAN: Latex table reporting the most affected and least affected averages for the CLAN variables and the difference.
+# 5) BEST: Latex table reporting best machine learning methods.
 
 # ML_Functions.R code is also required.
 
